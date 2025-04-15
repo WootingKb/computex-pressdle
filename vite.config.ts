b706d447e768ce47711e7145a45951f513181d51
Khang Nguyen <khang.nguyen5700@gmail.com>
2025-04-15 10:56:27 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // This needs to be the name of the repo to host the game on github pages
-  base: "/pressdle/",
   plugins: [react(), tailwindcss()],
 });
