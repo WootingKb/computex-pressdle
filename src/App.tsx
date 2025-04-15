46f70f9f909ec02a85dcbea9e69ce1fdaf68f588
Khang Nguyen <khang.nguyen5700@gmail.com>
2025-04-15 10:50:30 -0600

    try moving provider out of main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,10 @@
-import { Pressdle } from "./components/Pressdle";
-import { Header } from "./components/Header";
-import { ConnectDevice } from "./components/ConnectDevice";
-import { usePressdleContext } from "./lib/PressdleContext";
-import { useCallback, useEffect } from "react";
+import { PressdleProvider } from "./lib/PressdleContext";
+import { Root } from "./Root";
 
-function App() {
-  const { device, dispatch } = usePressdleContext();
-  const onDeviceConnect = useCallback(
-    (device: HIDDevice) => {
-      dispatch({ type: "CONNECT_DEVICE", payload: device });
-    },
-    [dispatch]
-  );
-
-  // Handle device disconnect
-  useEffect(() => {
-    const handler = (event: HIDConnectionEvent) => {
-      console.log("Device disconnected", event);
-
-      dispatch({ type: "DISCONNECT_DEVICE", payload: event.device });
-    };
-
-    navigator.hid.addEventListener("disconnect", handler);
-
-    // Cleanup
-    return () => {
-      navigator.hid.removeEventListener("disconnect", handler);
-    };
-  }, [dispatch]);
-
+export default function App() {
   return (
-    <div className="min-h-screen bg-zinc-900">
-      <Header />
-      <main className="flex flex-col items-center justify-center">
-        {device ? (
-          <Pressdle device={device} />
-        ) : (
-          <ConnectDevice onConnect={onDeviceConnect} />
-        )}
-      </main>
-    </div>
+    <PressdleProvider>
+      <Root />
+    </PressdleProvider>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { PressdleProvider } from "./lib/PressdleContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PressdleProvider>
-      <App />
-    </PressdleProvider>
+    <App />
   </StrictMode>
 );
