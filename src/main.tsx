1636f4da9e060ed30eca88e5581d663c0ad48842
Khang Nguyen <khang.nguyen5700@gmail.com>
2025-04-15 10:46:06 -0600

    try 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { PressdleProvider } from "./lib/PressdleContext/index.tsx";
+import App from "./App";
+import { PressdleProvider } from "./lib/PressdleContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
